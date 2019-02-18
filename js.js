d48aefb9a2956078e35691332aa4b41051980100
olkruglova <olkruhlova@gmail.com>
2019-02-18 12:50:34 +0100

    Add AJAX request

--- js.js
+++ js.js
@@ -1,41 +1,23 @@
+
+var $data = $('.data').css('display', 'none');
+
 function loadData() {
 
-    var $result = $('#search-result');
     var $errorText = $('.error-text');
-    var $data = $('.data');
-    /* var $wikiElem = $('#wikipedia-links');
-    var $nytHeaderElem = $('#nytimes-header');
-    var $nytElem = $('#nytimes-articles');
-    var $greeting = $('#greeting'); */
+    var $keyword = $('#searcher').val();
+    var $movieInfo = $('.movie-info');
 
-    // clear out old data before new request
-    /* $wikiElem.text("");
-    $nytElem.text(""); */
+    $('.description').css('display', 'none');
+    $('.hr-top').css('margin-top', '1%');
+    /***********************TMDb AJAX request*************************/
 
-    // load streetview
-    /* var gMapsUrl ='http://maps.googleapis.com/maps/api/streetview?size=900x700&location='
-    var streetStr = $('#street').val();
-    var cityStr = $('#city').val();
-    var address = streetStr + ',' + cityStr;
-    $body.append('<img class="bgimg" src="' + gMapsUrl + address + '&key=AIzaSyCPSWk9Gj32S52481URJOAI3kcqgx3KYzk&v=3">');
-
-    $greeting.text('So, you want to live at ' + address + '?'); */
-
-    /***********************NYtimes AJAX request*************************/
-
-    var TMDbUrl = 'https://api.themoviedb.org/3/movie/550?api_key=08fe9bb5845df49ca15cb554c6dcdc3d'
-    /* 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
-    nytimesUrl += '?' + $.param({
-    'q': cityStr,
-    'sort': "newest",
-    'api-key': "HBYuLY4wAQQEvfHkz0NzD4Edpz4Gw79I"
-    }); */
-
-
+    var TMDbUrl = 'https://api.themoviedb.org/3/search/movie?api_key=08fe9bb5845df49ca15cb554c6dcdc3d&language=en-US&page=1&query=' + $keyword;
+    //'https://api.themoviedb.org/3/search/' + $keyword + '?api_key=08fe9bb5845df49ca15cb554c6dcdc3d&page=1';
+    
     $.ajax({
         dataType: "json",
         url: TMDbUrl,
-        method:'GET',             //from TMDb we 'get' JSON data. 
+        method:'GET',             //from TMDb we 'get' JSON data.
         error: function() {
             $errorText.text('Movie information could not be loaded');
             console.log('error');
@@ -43,9 +25,30 @@ function loadData() {
     }).done(function(data){     //when TMDb API request succeed, then it works.
             console.log("success");
             console.log(data);
-            /* article = data.response.docs; */
-            $data.append('<img class = "image" src = "https://image.tmdb.org/t/p/w500/'+data.poster_path+'"> <span class="title">' + data.title + '</span>');
+            var movies = data.results;
+            var $data = $('.data').css('display', 'block');
+           for (var i=0; i < movies.length; i++) {
+                var movie = movies[i];
+                $data.append('<div class="movie-cover"><img class = "image" src = "https://image.tmdb.org/t/p/w200/'+movie.poster_path+'"><br><span class="title">' + movie.title + '</span></div>');
+            }
+            $data.append('<div id="buttons"><button>Next >>></button></div>')
+            $('.movie-cover').on('click', function() {
+                $(this).css('cursor', 'pointer');
+                $data.css('display', 'none');
+                $movieInfo.append(`
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img class = "image" src = "https://image.tmdb.org/t/p/w200/'`+movie.poster_path+`'">
+                        </div>
+                        <div class="col-md-8">
+                            <span>` + movie.overview + `</span>
+                        </div>
+                    </div>
+                `)
             });
-    }
+});
+}
 
 $('.submit').click(loadData);
+/* $('body').catchEnter('.input').on('enterkey',function(ev) { loadData }); */
+
